105c9ded9bdcffd9823364846bb0d891260969bf
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-10-04 09:01:10 +0300

    commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
