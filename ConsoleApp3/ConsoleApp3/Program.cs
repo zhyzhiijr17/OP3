8d592718c5e615b6c8d7d2391c084ed37785bc41
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-10-04 09:06:05 +0300

    commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿ class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число для таблиці множення: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Таблиця множення для числа {number}:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} x {i} = {number * i}");
+        }
+    }
+}
