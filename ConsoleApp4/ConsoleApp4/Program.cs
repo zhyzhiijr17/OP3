dc7eef4b3b6119a26e2d9f818ab6a61431b33c91
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-10-04 09:24:17 +0300

    commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число для обчислення факторіалу: ");
+        int number = int.Parse(Console.ReadLine());
+
+        long factorial = 1;
+
+        for (int i = 2; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"Факторіал числа {number} = {factorial}");
+    }
+}
