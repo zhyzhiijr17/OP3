5e849df204e7b9e6a1572c1d530500da3b83d323
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-10-04 09:27:59 +0300

    commit

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть число для перетворення в двійкову систему: ");
+        int number = int.Parse(Console.ReadLine());
+
+        string binary = Convert.ToString(number, 2);
+
+        Console.WriteLine($"Число {number} в двійковій системі: {binary}");
+    }
+}
