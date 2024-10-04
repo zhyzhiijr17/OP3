d8ce6cc8bec403bf5a2924f75324595e9bcf0e3f
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-10-04 09:26:04 +0300

    commit

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        for (int i = 2; i <= 50; i++)
+        {
+            if (IsPrime(i))
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine("Сума всіх простих чисел від 1 до 50: " + sum);
+    }
+
+    // Метод для перевірки, чи є число простим
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
