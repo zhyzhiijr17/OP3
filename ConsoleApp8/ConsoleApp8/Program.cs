835a6b0e950632b428492de529e805ad0773bafe
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-10-04 09:34:17 +0300

    commit

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть перше число:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int gcd = FindGCD(num1, num2);
+
+        Console.WriteLine($"Найбільший спільний дільник (НСД) чисел {num1} і {num2} дорівнює {gcd}.");
+    }
+
+    static int FindGCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.Abs(a); // Повертаємо абсолютне значення
+    }
+}
