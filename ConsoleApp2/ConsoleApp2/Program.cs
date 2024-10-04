ace11dc97f42e2726585c0a70a6f8e5468b6455f
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-10-04 09:03:00 +0300

    commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine("Сума чисел від 1 до 100: " + sum);
+    }
+}
